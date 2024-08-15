fc0910ec30a36b2a9f03c7445498128041d3e3e8
nooraiym <140681537+nooraiym@users.noreply.github.com>
2024-08-15 17:01:54 +0500

    fix: adding description to items

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,4 +1,4 @@
-const { Device } = require('../models/models')
+const { Device, DeviceInfo } = require('../models/models')
 const ApiError = require('../error/ApiError')
 const uuid = require('uuid')
 const path = require('path')
@@ -6,11 +6,10 @@ const path = require('path')
 class DeviceController {
   async create(req, res, next) {
     try {
-      const { name, price, brandId, typeId, info } = req.body
+      let { name, price, brandId, typeId, info } = req.body
       const { img } = req.files
       let fileName = uuid.v4() + '.jpg'
       img.mv(path.resolve(__dirname, '..', 'static', fileName))
-
       const device = await Device.create({
         name,
         price,
@@ -18,6 +17,18 @@ class DeviceController {
         typeId,
         img: fileName,
       })
+
+      if (info) {
+        info = JSON.parse(info)
+        info.forEach(i =>
+          DeviceInfo.create({
+            title: i.title,
+            description: i.description,
+            deviceId: device.id,
+          })
+        )
+      }
+
       return res.json(device)
     } catch (error) {
       next(ApiError.badRequest(error.message))
@@ -55,7 +66,14 @@ class DeviceController {
     }
     return res.json(devices)
   }
-  async getOne(req, res) {}
+  async getOne(req, res) {
+    const { id } = req.params
+    const device = await Device.findOne({
+      where: { id },
+      include: [{ model: DeviceInfo, as: 'info' }],
+    })
+    return res.json(device)
+  }
 }
 
 module.exports = new DeviceController()
